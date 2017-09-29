3d19f03261199894ab4b1157af645ad0f46ce134
luyunhai <luyunhai@58ganji.com>
2017-09-29 15:36:04 +0800

    修改Home，add Image

---
+++ src/libs/Constants.js
@@ -0,0 +1,8 @@
+export const Navigation = {
+    config: {
+        navigationBarTitleText: 'Home',
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#000',
+        navigationBarTextStyle: 'white'
+    }
+};
